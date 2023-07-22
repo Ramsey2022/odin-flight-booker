7426103b686543f0bf0691b65aec386b0e19efcd
Ramsey2022 <abovethelotus@yahoo.com>
2023-07-22 13:25:28 -0700

    added flight and airport models

---
+++ db/migrate/20230722201751_create_flights.rb
@@ -0,0 +1,12 @@
+class CreateFlights < ActiveRecord::Migration[7.0]
+  def change
+    create_table :flights do |t|
+      t.bigint :origin_id
+      t.bigint :destination_id
+      t.date :departure_date
+      t.time :departure_time
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Airport.find_or_create_by(code: "SFO", location: "San Francisco, CA")
+Airport.find_or_create_by(code: "NYC", location: "New York City, NY")
+Airport.find_or_create_by(code: "ATL", location: "Atlanta, GA")
+Airport.find_or_create_by(code: "ORD", location: "Chicago, IL")
+Airport.find_or_create_by(code: "LAX", location: "Los Angeles, CA")
+Airport.find_or_create_by(code: "DFW", location: "Dallas, TX")
+Airport.find_or_create_by(code: "MCO", location: "Orlando, FL")
+Airport.find_or_create_by(code: "DEN", location: "Denver, CO")
+
+start = Time.zone.today
+finish = Time.zone.today + 31.days
+
+(start..finish).each do |date|
+  flight_generator = FlightGenerator.new(date)
+  flight_generator.call
+end
